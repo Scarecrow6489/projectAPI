48024c6d8cf238c16df5a4ad5da9afdf27cbeb15
Shubham Maynal <shubhammaynal1@gmail.com>
2019-03-29 11:32:26 +0530

    Angular v6 modifications

--- server.js
+++ server.js
@@ -1,27 +1,48 @@
-const path = require('path');
+// const path = require('path');
+// const express = require('express');
+// const app = express();
+
+// let env = process.env.NODE_ENV || 'development';
+
+// let forceSSL = (req, res, next) => {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//         return res.redirect(['https://', req.get('Host'), req.url].join(''));
+//     }
+//     return next();
+// };
+
+// if (env === 'production') {
+//     app.use(forceSSL);
+// }
+
+// // Serve static files
+// app.use(express.static(__dirname + '/dist/the_story_teller'));
+
+// // Send all requests to index.html
+// app.get('/*', function (req, res) {
+//     res.sendFile(path.join(__dirname + '/dist/the_story_teller/index.html'));
+// });
+
+// // default Heroku port
+// app.listen(process.env.PORT || 5000);
+
+
+
+
+
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-let env = process.env.NODE_ENV || 'development';
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/the_story_teller'));
 
-let forceSSL = (req, res, next) => {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-        return res.redirect(['https://', req.get('Host'), req.url].join(''));
-    }
-    return next();
-};
-
-if (env === 'production') {
-    app.use(forceSSL);
-}
-
-// Serve static files
-app.use(express.static(__dirname + '/dist/the_story_teller'));
-
-// Send all requests to index.html
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/the_story_teller/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname,'/dist/the_story_teller/index.html'));
 });
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
